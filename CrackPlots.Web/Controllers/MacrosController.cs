fbb12a2524aea04d0dff4ca185cc89f47dbd2b34
dale-schmidt <dwhale84@gmail.com>
2020-04-18 14:06:04 -0700

    macro worksheet

---
+++ CrackPlots.Web/Controllers/MacrosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForeSight.Web.Controllers
+{
+    public class MacrosController : BaseController
+    {
+        // GET: Macros
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
